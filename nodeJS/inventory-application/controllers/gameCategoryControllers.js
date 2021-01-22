b4baa3bd372091547e6b06ab8423c5aaf466a1e6
Nick Nemtcev <nnem02161999@gmail.com>
2021-01-22 14:19:23 -0500

    Create controllers for video games and video game categories

---
+++ nodeJS/inventory-application/controllers/gameCategoryControllers.js
@@ -0,0 +1,21 @@
+const getGameCategories = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const getGameCategoryDetail = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const editGameCategory = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const deleteGameCategory = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const createNewGameCategory = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+module.exports = { getGameCategories, getGameCategoryDetail, editGameCategory, deleteGameCategory, createNewGameCategory };
---
+++ nodeJS/inventory-application/controllers/gameControllers.js
@@ -0,0 +1,22 @@
+const getGameDetail = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const getGames = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const editGame = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const deleteGame = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+const createNewGame = function(req, res, next) {
+    res.send('FINISH THIS CONTROLLER LATER');
+};
+
+module.exports = { getGameDetail, getGames, editGame, deleteGame, createNewGame };
+
